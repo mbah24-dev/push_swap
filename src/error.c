5c645cf793cbe435d4d3d76239891c1a56dcd36a
BAH Mamad <mbah@student.42lyon.fr>
2024-12-21 01:00:02 +0100

    feat(utils): ajout des fonctions utilitaires pour parser et valider les valeurs utilisateur
    - Implémentation des fonctions pour transformer les arguments utilisateur en un double tableau de strings.
    - Vérification des erreurs de conversion via  (valeurs non valides ou hors des limites d'un int).
    - Ajout d'une vérification pour détecter la présence de doublons dans les valeurs.
    - Ces outils permettent une validation robuste des entrées avant l'initialisation des piles.

---
+++ src/error.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 22:21:36 by mbah              #+#    #+#             */
+/*   Updated: 2024/12/20 23:45:56 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	has_value_duplicates(char **values)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (values[i])
+	{
+		j = i + 1;
+		while (values[j])
+		{
+			if (ft_strcmp(values[i], values[j]) == 0)
+				return (1);
+			j = j + 1;
+		}
+		i = i + 1;
+	}
+	return (0);
+}
+
+/* evite le signe + a la ligne 49 */
+int	values_is_integer(char **values)
+{
+	int		i;
+
+	i = 0;
+	while (values[i])
+	{
+		if (values[i][0] != '+')
+		{
+			if (ft_strcmp(values[i], ft_itoa(ft_atoi(values[i]))) != 0)
+				return (0);
+		}
+		else
+		{
+			if (ft_strcmp(values[i] + 1, ft_itoa(ft_atoi(values[i]))) != 0)
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	an_error_occured(char **values)
+{
+	if (has_value_duplicates(values) || !values_is_integer(values))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ src/utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/14 16:15:44 by mbah              #+#    #+#             */
+/*   Updated: 2024/12/18 23:32:14 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i = i + 1;
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*res;
+	int		i;
+
+	i = 0;
+	res = (char *) malloc((ft_strlen(str) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	while (str[i])
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		res = res * 10 + (str[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
+
+size_t	ft_count_word(char *str, char sp)
+{
+	size_t	number;
+	int		i;
+
+	number = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] && str[i] != sp)
+		{
+			number = number + 1;
+			while (str[i] && str[i] != sp)
+				i++;
+		}
+		else
+			i++;
+	}
+	return (number);
+}
+
+size_t	ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dst && !src)
+		return (0);
+	if (size > 0)
+	{
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
