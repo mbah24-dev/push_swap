7df311190f75f61839ce2ec14d466974e383bdd8
BAH Mamad <mbah@student.42lyon.fr>
2024-12-22 19:38:11 +0100

    feat(utils) : ajout des implémentations de ft_split et ft_itoa pour le traitement des arguments
    - Implémente  pour diviser les arguments en valeurs individuelles en se basant sur les espaces.
    - Ajoute  pour gérer la conversion d'entiers en chaînes, facilitant la détection des erreurs lors du traitement des arguments.
    - Introduit une logique de validation pour vérifier si une valeur est dans la plage d’un  en comparant la chaîne originale () avec le résultat de .
    - Retourne une erreur (1) en cas d'échec de la conversion afin d'assurer une validation robuste des arguments.
    
    Ces utilitaires sont essentiels pour gérer le parsing des entrées et le contrôle des erreurs dans le projet .

---
+++ src/ft_itoa.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 23:45:44 by mbah              #+#    #+#             */
+/*   Updated: 2024/12/19 02:11:06 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && (s1[i] == s2[i]))
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+static int	ft_len(int number)
+{
+	int	len;
+
+	len = 1;
+	if (number < 0)
+		len++;
+	while (number / 10 != 0)
+	{
+		number /= 10;
+		len++;
+	}
+	return (len);
+}
+
+static void	ft_itoa_recursive(long number, char *res, int *idx)
+{
+	if (number >= 10)
+	{
+		ft_itoa_recursive(number / 10, res, idx);
+		ft_itoa_recursive(number % 10, res, idx);
+	}
+	else
+	{
+		res[*idx] = number + 48;
+		(*idx) += 1;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int		i;
+	long	number;
+
+	i = 0;
+	number = n;
+	res = (char *) malloc(sizeof(char) * (ft_len(number) + 1));
+	if (!res)
+		return (NULL);
+	if (number < 0)
+	{
+		res[i] = '-';
+		i++;
+		number *= -1 ;
+	}
+	ft_itoa_recursive(number, res, &i);
+	res[i] = '\0';
+	return (res);
+}
---
+++ src/ft_split.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 15:21:48 by mbah              #+#    #+#             */
+/*   Updated: 2024/12/18 20:49:55 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	ft_free_all(char **str, int *i)
+{
+	while (*i > 0)
+	{
+		free(str[*i]);
+		str[*i] = (NULL);
+		*i = *i - 1;
+	}
+}
+
+static size_t	ft_wordsize(char *str, char sp)
+{
+	size_t	number;
+
+	number = 0;
+	while (str[number] && str[number] != sp)
+		number = number + 1;
+	return (number);
+}
+
+char	**ft_split(char *str, char sp)
+{
+	char	**res;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	res = (char **) malloc(sizeof(char *) * (ft_count_word(str, sp) + 1));
+	if (!res)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] != sp)
+		{
+			res[j] = malloc(sizeof(char) * (ft_wordsize(str + i, sp) + 1));
+			if (!res[j])
+				return (ft_free_all(res, &j), NULL);
+			ft_strlcpy(res[j], str + i, ft_wordsize(str + i, sp) + 1);
+			j = j + 1;
+			i = i + ft_wordsize(str + i, sp);
+		}
+		else
+			i = i + 1;
+	}
+	res[j] = (NULL);
+	return (res);
+}
