230d5965594a9b3f57d88022d3a503b7267073c3
BAH Mamad <mbah@student.42lyon.fr>
2024-12-29 13:51:11 +0100

    feat(main): ajout du point d'entree principal du programme
    - Implementation de la fonction main qui orchestre l'execution globale de l'application.
    - Ajout des fonctions utilitaires :
      - free_recursively : libere un tableau de chaines de caracteres et reinitialise les pointeurs.
      - free_all : libere les piles stack_a et stack_b tout en reinitialisant leurs pointeurs.
      - ft_putstr_fd : affiche une chaine de caracteres sur un descripteur donne.
    - Initialisation et traitement des donnees :
      - Extraction des valeurs avec get_values_in_str et verification des erreurs via an_error_occured.
      - Conversion et allocation des valeurs finales avec get_final_values et fill_stack.
    - Ajout de la logique pour trier les piles :
      - Tri des petites piles avec sort_small_stack.
      - Gestion des grandes piles avec sort_large_stacks.
    - Gestion de la memoire :
      - Liberation des ressources dynamiques (valeurs, piles, etc.) avant la sortie.
    
    Ce fichier constitue la base du programme en reliant les differentes fonctionnalites.

---
+++ src/main.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 14:57:24 by mbah              #+#    #+#             */
+/*   Updated: 2024/12/29 13:48:07 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	free_recursively(char **str)
+{
+	int	i;
+
+	i = -1;
+	while (str[++i])
+	{
+		free(str[i]);
+		str[i] = (NULL);
+	}
+	free(str);
+	str = (NULL);
+}
+
+void	free_all(void *stack_a, void *stack_b)
+{
+	free(stack_a);
+	stack_a = (NULL);
+	free(stack_b);
+	stack_b = (NULL);
+}
+
+void	ft_putstr_fd(char *str, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(fd, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	char	**res;
+	int		*values;
+	int		size;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+
+	(void) argc;
+	if (argc == 1)
+		return (0);
+	res = get_values_in_str(argv + 1, ' ');
+	an_error_occured(res);
+	values = get_final_values(argv + 1, ' ', &size);
+	stack_a = fill_stack(values, &stack_b, size);
+	free(values);
+	values = (NULL);
+	if (get_stack_size(stack_a) <= 5)
+		sort_small_stack(stack_a, stack_b);
+	else
+		sort_large_stacks(stack_a, stack_b);
+	free_all(stack_a, stack_b);
+	return (0);
+}
