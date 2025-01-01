5be2f590c7d596e31adc602e69c6607af6ab322e
BAH Mamad <mbah@student.42lyon.fr>
2025-01-01 17:22:38 +0100

    feat: add stack operations for rotate and swap/push
    - Added app.stack_op_rotate.c to handle stack rotation operations.
    - Added app.stack_op_swap_push.c to implement swap and push operations for the stack.
    
    These changes introduce core functionalities for stack manipulation in the application.

---
+++ src/app/app.stack_op_rotate.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   app.stack_op_rotate.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 21:27:31 by mbah              #+#    #+#             */
+/*   Updated: 2024/12/27 22:45:19 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+void	rotate_(t_stack *stack_, char type)
+{
+	t_stack	temp;
+	int		i;
+
+	i = 0;
+	if (stack_[0].key != -1 && stack_[1].key != -1)
+	{
+		temp.value = stack_[0].value;
+		while (stack_[i].key != -1)
+		{
+			stack_[i].value = stack_[i + 1].value;
+			i++;
+		}
+		stack_[i - 1].value = temp.value;
+		reset_keys(stack_);
+		if (type == 'a')
+			write(1, "ra\n", 3);
+		else if (type == 'b')
+			write(1, "rb\n", 3);
+	}
+}
+
+void	rotate_a_rotate_b(t_stack *stack_a, t_stack *stack_b)
+{
+	write(1, "rr\n", 3);
+	rotate_(stack_a, 'r');
+	rotate_(stack_b, 'r');
+}
+
+void	reverse_rotate_(t_stack *stack_, char type)
+{
+	t_stack	temp;
+	size_t	size;
+
+	if (stack_[0].key != -1 && stack_[1].key != -1)
+	{
+		size = get_stack_size(stack_);
+		temp.value = stack_[size - 1].value;
+		while (size > 1)
+		{
+			stack_[size - 1].value = stack_[size - 2].value;
+			size--;
+		}
+		stack_[0].value = temp.value;
+		reset_keys(stack_);
+		if (type == 'a')
+			write(1, "rra\n", 4);
+		else if (type == 'b')
+			write(1, "rrb\n", 4);
+	}
+}
+
+void	rra_rrb(t_stack *stack_a, t_stack *stack_b)
+{
+	write(1, "rrr\n", 4);
+	reverse_rotate_(stack_a, 'r');
+	reverse_rotate_(stack_b, 'r');
+}
---
+++ src/app/app.stack_op_swap_push.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   app.stack_op_swap_push.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 18:51:14 by mbah              #+#    #+#             */
+/*   Updated: 2024/12/27 22:44:30 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+int	is_stack_sorted(t_stack *stack)
+{
+	int	i;
+
+	i = 0;
+	if (get_stack_size(stack) <= 1)
+		return (1);
+	while (stack[i].key != -1 && stack[i + 1].key != -1)
+	{
+		if (stack[i].value > stack[i + 1].value)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	swap_(t_stack *stack_, char type)
+{
+	t_stack	temp;
+
+	if (get_stack_size(stack_) >= 2)
+	{
+		temp = stack_[0];
+		stack_[0].value = stack_[1].value;
+		stack_[1].value = temp.value;
+		if (type == 'a')
+			write(1, "sa\n", 3);
+		else if (type == 'b')
+			write(1, "sb\n", 3);
+	}
+}
+
+void	swap_a_swap_b(t_stack *stack_a, t_stack *stack_b)
+{
+	write(1, "sa\n", 3);
+	swap_(stack_a, 'a');
+	write(1, "sb\n", 3);
+	swap_(stack_b, 'b');
+}
+
+void	push_(t_stack *stack_src, t_stack *stack_dst, char type)
+{
+	size_t	dst_size;
+	int		i;
+
+	if (stack_src[0].key != -1)
+	{
+		dst_size = get_stack_size(stack_dst) + 1;
+		while (dst_size != 0)
+		{
+			stack_dst[dst_size] = stack_dst[dst_size - 1];
+			dst_size--;
+		}
+		stack_dst[0] = stack_src[0];
+		i = -1;
+		while (stack_src[++i].key != -1)
+			stack_src[i].value = stack_src[i + 1].value;
+		stack_src[i - 1] = stack_src[i];
+		reset_keys(stack_dst);
+		reset_keys(stack_src);
+		if (type == 'a')
+			write(1, "pa\n", 3);
+		else if (type == 'b')
+			write(1, "pb\n", 3);
+	}
+}
