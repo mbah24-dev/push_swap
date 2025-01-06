4906baa9f75de47f7a1f6b7645db1877c1ac1f62
BAH Mamad <mbah@student.42lyon.fr>
2025-01-06 01:52:43 +0100

    refactor: clean up utils in app.sort_large_stacks_utils.c
    Streamlined the utility functions used in sort_large_stacks.
    Removed redundant or inefficient logic, improving readability and performance.

--- src/app/app.sort_large_stacks_utils.c
+++ src/app/app.sort_large_stacks_utils.c
@@ -6,11 +6,35 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 16:03:42 by mbah              #+#    #+#             */
-/*   Updated: 2024/12/28 02:44:44 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/06 01:21:00 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
+
+t_optimal_move	optimal_move_cost(t_stack *stack_, t_stack val)
+{
+	int				nb_rotate;
+	int				nb_rev_rotate;
+	size_t			stack_size;
+	t_optimal_move	opt_move;
+
+	stack_size = get_stack_size(stack_);
+	nb_rotate = val.key;
+	nb_rev_rotate = stack_[stack_size - 1].key - val.key + 1;
+	if (nb_rotate <= nb_rev_rotate)
+	{
+		if (nb_rotate == 0)
+			opt_move.is_rotate = 3;
+		else
+			opt_move.is_rotate = 1;
+		opt_move.cost = nb_rotate;
+		return (opt_move);
+	}
+	opt_move.cost = nb_rev_rotate;
+	opt_move.is_rotate = 0;
+	return (opt_move);
+}
 
 t_stack	get_max_value(const t_stack *stack_)
 {
@@ -39,39 +63,7 @@ int	is_min_or_max(t_stack *stack_, t_stack element)
 	return (0);
 }
 
-int	move_to_top_cost(t_stack *stack_, t_stack val)
-{
-	int		nb_rotate;
-	int		nb_rev_rotate;
-	size_t	stack_size;
-
-	stack_size = get_stack_size(stack_);
-	nb_rotate = val.key;
-	nb_rev_rotate = stack_[stack_size - 1].key - val.key + 1;
-	if (nb_rotate <= nb_rev_rotate)
-		return (nb_rotate);
-	return (nb_rev_rotate);
-}
-
-int	calculate_push_cost(t_stack *stack_a, t_stack *stack_b, t_stack elemnt)
-{
-	int		cost_a;
-	int		cost_b;
-	t_stack	next_smaller;
-
-	cost_a = move_to_top_cost(stack_a, elemnt);
-	if (is_min_or_max(stack_b, elemnt))
-		cost_b = (move_to_top_cost(stack_b, get_max_value(stack_b)));
-	else
-	{
-		next_smaller = find_next_(stack_b, elemnt, "smaller");
-		cost_b = move_to_top_cost(stack_b, next_smaller);
-	}
-	return (cost_a + cost_b + 1);
-}
-
-void	merge_stacks(t_stack *stack_a, t_stack *stack_b,
-	int *min_cost, int *max_cost)
+void	merge_stacks(t_stack *stack_a, t_stack *stack_b)
 {
 	t_stack	next_larger;
 	size_t	size_a;
@@ -81,14 +73,7 @@ void	merge_stacks(t_stack *stack_a, t_stack *stack_b,
 	{
 		size_a = get_stack_size(stack_a);
 		if (is_min_or_max(stack_a, stack_b[0]))
-		{
-			*min_cost = move_to_top_cost(stack_a, get_min_value(stack_a));
-			*max_cost = move_to_top_cost(stack_a, get_max_value(stack_a));
-			if (*max_cost <= *min_cost)
-				optimal_min_move(stack_a, 'a', get_max_value(stack_a), size_a);
-			else
-				optimal_min_move(stack_a, 'a', get_min_value(stack_a), size_a);
-		}
+			optimal_min_move(stack_a, 'a', get_min_value(stack_a), size_a);
 		else
 		{
 			next_larger = find_next_(stack_a, stack_b[0], "larger");
