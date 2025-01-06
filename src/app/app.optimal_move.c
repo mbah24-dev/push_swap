9210164eb1f196402dd01f96ac0976173e013bea
BAH Mamad <mbah@student.42lyon.fr>
2025-01-06 01:51:05 +0100

    refactor: optimize movement logic in app.optimal_move.c
    Refactored the optimal move selection to prioritize rr (rotate both) over individual ra and rb, reducing operation cost.
    Improved the decision-making process for handling rotations.

--- src/app/app.optimal_move.c
+++ src/app/app.optimal_move.c
@@ -6,50 +6,27 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 19:52:41 by mbah              #+#    #+#             */
-/*   Updated: 2025/01/03 14:24:18 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/06 01:29:18 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
-
-t_optimal_move	optimal_move_cost(t_stack *stack_, t_stack val)
-{
-	int				nb_rotate;
-	int				nb_rev_rotate;
-	size_t			stack_size;
-	t_optimal_move	opt_move;
-
-	stack_size = get_stack_size(stack_);
-	nb_rotate = val.key;
-	nb_rev_rotate = stack_[stack_size - 1].key - val.key + 1;
-	if (nb_rotate <= nb_rev_rotate)
-	{
-		if (nb_rotate == 0)
-			opt_move.is_rotate = 3;
-		else
-			opt_move.is_rotate = 1;
-		opt_move.cost = nb_rotate;
-		return (opt_move);
-	}
-	opt_move.cost = nb_rev_rotate;
-	opt_move.is_rotate = 0;
-	return (opt_move);
-}
 
 int	get_final_cost_a_b(t_optimal_move *cost_a_b)
 {
 	if (cost_a_b[0].is_rotate == cost_a_b[1].is_rotate)
 	{
 		if (cost_a_b[0].cost < cost_a_b[1].cost)
-			return ((cost_a_b[0].cost + 1) + 1);
+			return (cost_a_b[1].cost + 1);
 		else if (cost_a_b[0].cost > cost_a_b[1].cost)
-			return ((cost_a_b[1].cost + 1) + 1);
+			return (cost_a_b[0].cost + 1);
 		return ((cost_a_b[0].cost + 1));
 	}
 	return (cost_a_b[0].cost + cost_a_b[1].cost + 1);
 }
 
-t_optimal_move	*calc_opt_push_cost(t_stack *stack_a, t_stack *stack_b, t_stack elemnt)
+t_optimal_move	*calc_opt_push_cost(t_stack *stack_a, t_stack *stack_b,
+	t_stack elemnt)
 {
 	t_optimal_move		*cost_a_b;
 	t_stack				next_smaller;
@@ -68,10 +45,10 @@ t_optimal_move	*calc_opt_push_cost(t_stack *stack_a, t_stack *stack_b, t_stack e
 	return (cost_a_b);
 }
 
-/*optimal_min_move*/
-void	optimal_move_together(t_stack *stack_a, t_stack *stack_b, int is_rotate, int nb)
+void	optimal_move_together(t_stack *stack_a, t_stack *stack_b, int is_rotate,
+	int nb)
 {
-	int	i;;
+	int	i;
 
 	i = 0;
 	if (is_rotate == 1)
@@ -92,32 +69,48 @@ void	optimal_move_together(t_stack *stack_a, t_stack *stack_b, int is_rotate, in
 	}
 }
 
-void	optimal_move_and_push_to_b(t_stack *stack_a, t_stack *stack_b, t_stack element)
+void	process_same_op(t_stack *s_a, t_stack *s_b, t_stack elt,
+	t_optimal_move *cost_a_b)
+{
+	t_stack			next_smaller;
+
+	next_smaller = new_element(0, -1);
+	if (cost_a_b[0].cost < cost_a_b[1].cost)
+	{
+		optimal_move_together(s_a, s_b, cost_a_b[0].is_rotate,
+			cost_a_b[0].cost);
+		if (is_min_or_max(s_b, elt))
+			next_smaller = get_max_value(s_b);
+		else
+			next_smaller = find_next_(s_b, elt, "smaller");
+		optimal_min_move(s_b, 'b', next_smaller, get_stack_size(s_b));
+	}
+	else if (cost_a_b[0].cost > cost_a_b[1].cost)
+	{
+		optimal_move_together(s_a, s_b, cost_a_b[1].is_rotate,
+			cost_a_b[1].cost);
+		optimal_min_move(s_a, 'a', elt, get_stack_size(s_a));
+	}
+	else
+		optimal_move_together(s_a, s_b, cost_a_b[0].is_rotate,
+			cost_a_b[0].cost);
+}
+
+void	optimal_move_and_push_to_b(t_stack *stack_a, t_stack *stack_b,
+	t_stack element)
 {
 	t_stack			next_smaller;
 	t_optimal_move	*cost_a_b;
 
-	if (is_min_or_max(stack_b, element))
-		next_smaller = get_max_value(stack_b);
-	else
-		next_smaller = find_next_(stack_b, element, "smaller");
 	cost_a_b = calc_opt_push_cost(stack_a, stack_b, element);
 	if (cost_a_b[0].is_rotate == cost_a_b[1].is_rotate)
-	{
-		if (cost_a_b[0].cost < cost_a_b[1].cost)
-		{
-			optimal_move_together(stack_a, stack_b, cost_a_b[0].is_rotate, cost_a_b[0].cost);
-			optimal_move_together(stack_a, stack_b, cost_a_b[0].is_rotate, (cost_a_b[1].cost - cost_a_b[0].cost));
-		}
-		else if (cost_a_b[0].cost > cost_a_b[1].cost)
-		{
-			optimal_move_together(stack_a, stack_b, cost_a_b[1].is_rotate, cost_a_b[1].cost);
-			optimal_move_together(stack_a, stack_b, cost_a_b[1].is_rotate, (cost_a_b[0].cost - cost_a_b[1].cost));
-		}
-		optimal_move_together(stack_a, stack_b, cost_a_b[0].is_rotate, cost_a_b[0].cost);
-	}
+		process_same_op(stack_a, stack_b, element, cost_a_b);
 	else
 	{
+		if (is_min_or_max(stack_b, element))
+			next_smaller = get_max_value(stack_b);
+		else
+			next_smaller = find_next_(stack_b, element, "smaller");
 		optimal_min_move(stack_a, 'a', element, get_stack_size(stack_a));
 		optimal_min_move(stack_b, 'b', next_smaller, get_stack_size(stack_b));
 	}
