1529066b86748268f540472c676931f904e2a76e
BAH Mamad <mbah@student.42lyon.fr>
2025-01-06 01:49:36 +0100

    refactor: modify push_swap.h for improved clarity and optimization
    Cleaned up and restructured push_swap.h to align with new changes.
    Added or removed necessary function declarations to reflect recent updates.

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mbah <mbah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 14:03:47 by mbah              #+#    #+#             */
-/*   Updated: 2024/12/28 15:08:51 by mbah             ###   ########.fr       */
+/*   Updated: 2025/01/06 01:19:19 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,74 +25,90 @@ struct	s_stack
 
 typedef struct s_stack	t_stack;
 
+/*=========== (s_optimal_move) ===========*/
+
+typedef struct s_optimal_move
+{
+	int	cost;
+	int	is_rotate;
+}	t_optimal_move;
+
+t_optimal_move	optimal_move_cost(t_stack *stack_, t_stack val);
+int				get_final_cost_a_b(t_optimal_move *cost_a_b);
+t_optimal_move	*calc_opt_push_cost(t_stack *stack_a, t_stack *stack_b,
+					t_stack elemnt);
+void			optimal_move_together(t_stack *stack_a, t_stack *stack_b,
+					int is_rotate, int nb);
+void			optimal_move_and_push_to_b(t_stack *stack_a, t_stack *stack_b,
+					t_stack element);
+void			process_same_op(t_stack *s_a, t_stack *s_b, t_stack elt,
+					t_optimal_move *cost_a_b);
+
 /*=========== (utils functions) ===========*/
 
-size_t	ft_strlen(char *str);
-char	*ft_strdup(char *str);
-int		ft_atoi(char *str);
-size_t	ft_count_word(char *str, char sp);
-size_t	ft_strlcpy(char *dst, char *src, size_t size);
-char	**ft_split(char *str, char sp);
+size_t			ft_strlen(char *str);
+char			*ft_strdup(char *str);
+int				ft_atoi(char *str);
+size_t			ft_count_word(char *str, char sp);
+size_t			ft_strlcpy(char *dst, char *src, size_t size);
+char			**ft_split(char *str, char sp);
 
 /*=========== (parse_argv functions) ===========*/
 
-size_t	number_of_value(char **argv, char sp);
-int		deep_copy(char **dst, char **src, int start);
-char	**get_values_in_str(char **av, char sp);
-int		*get_final_values(char **values, char sp, int *size);
+size_t			number_of_value(char **argv, char sp);
+int				deep_copy(char **dst, char **src, int start);
+char			**get_values_in_str(char **av, char sp);
+int				*get_final_values(char **values, char sp, int *size);
 
 /*=========== (error functions) ===========*/
-int		ft_strcmp(char *s1, char *s2);
-int		values_is_integer(char **values);
-char	*ft_itoa(int n);
-void	an_error_occured(char **values);
+int				ft_strcmp(char *s1, char *s2);
+int				values_is_integer(char **values);
+char			*ft_itoa(int n);
+void			an_error_occured(char **values);
 
 /*=========== (init_stack functions) ===========*/
-t_stack	new_element(int value, int key);
-size_t	get_stack_size(t_stack *stack);
-t_stack	*fill_stack(int *values, t_stack **stack_b, int stack_size);
-void	stack_status(t_stack *stack, int status);
+t_stack			new_element(int value, int key);
+size_t			get_stack_size(t_stack *stack);
+t_stack			*fill_stack(int *values, t_stack **stack_b, int stack_size);
+void			stack_status(t_stack *stack, int status);
 
 /*=========== (stack_op_swap_push functions) ===========*/
-int		is_stack_sorted(t_stack *stack);
-void	swap_(t_stack *stack_, char type);
-void	swap_a_swap_b(t_stack *stack_a, t_stack *stack_b);
-void	push_(t_stack *stack_src, t_stack *stack_dst, char type);
+int				is_stack_sorted(t_stack *stack);
+void			swap_(t_stack *stack_, char type);
+void			swap_a_swap_b(t_stack *stack_a, t_stack *stack_b);
+void			push_(t_stack *stack_src, t_stack *stack_dst, char type);
 
 /*=========== (stack_op_rotate functions) ===========*/
-void	rotate_(t_stack *stack_, char type);
-void	rotate_a_rotate_b(t_stack *stack_a, t_stack *stack_b);
-void	reverse_rotate_(t_stack *stack_, char type);
-void	rra_rrb(t_stack *stack_a, t_stack *stack_b);
+void			rotate_(t_stack *stack_, char type);
+void			rotate_a_rotate_b(t_stack *stack_a, t_stack *stack_b);
+void			reverse_rotate_(t_stack *stack_, char type);
+void			rra_rrb(t_stack *stack_a, t_stack *stack_b);
 
 /*=========== (stack_sort_small functions) ===========*/
-void	sort_two_elements(t_stack *stack_a, char type);
-void	sort_three_elements(t_stack *stack_a);
-void	sort_four_elements(t_stack *stack_a, t_stack *stack_b);
-void	sort_five_elements(t_stack *stack_a, t_stack *stack_b);
-int		sort_small_stack(t_stack *stack_a, t_stack *stack_b);
+void			sort_two_elements(t_stack *stack_a, char type);
+void			sort_three_elements(t_stack *stack_a);
+void			sort_four_elements(t_stack *stack_a, t_stack *stack_b);
+void			sort_five_elements(t_stack *stack_a, t_stack *stack_b);
+int				sort_small_stack(t_stack *stack_a, t_stack *stack_b);
 
 /*=========== (stack_sort_small_utils functions) ===========*/
-void	reset_keys(t_stack *stack_);
-t_stack	get_min_value(t_stack *stack_);
-void	move_min_to_top(t_stack *stack_, char type);
-void	optimal_min_move(t_stack *stack_, char type, t_stack min, int sz);
+void			reset_keys(t_stack *stack_);
+t_stack			get_min_value(t_stack *stack_);
+void			move_min_to_top(t_stack *stack_, char type);
+void			optimal_min_move(t_stack *stack_, char type, t_stack min,
+					int sz);
 
 /*=========== (sort_large_stack et _utils functions) ===========*/
-t_stack	get_max_value(const t_stack *stack_);
-int		calculate_push_cost(t_stack *stack_a, t_stack *stack_b, t_stack elemnt);
-void	merge_stacks(t_stack *stack_a, t_stack *stack_b,
-			int *min_cost, int *max_cost);
-int		is_min_or_max(t_stack *stack_, t_stack element);
-int		move_to_top_cost(t_stack *stack_, t_stack val);
-void	compare_and_push_min_cost(t_stack *stack_a, t_stack *stack_b);
-void	move_and_push_to_b(t_stack *stack_a, t_stack *stack_b, t_stack element);
-int		sort_large_stacks(t_stack *stack_a, t_stack *stack_b);
-t_stack	find_next_(const t_stack *stack_, t_stack elemnt, char *type);
+t_stack			get_max_value(const t_stack *stack_);
+void			merge_stacks(t_stack *stack_a, t_stack *stack_b);
+int				is_min_or_max(t_stack *stack_, t_stack element);
+void			compare_and_push_min_cost(t_stack *stack_a, t_stack *stack_b);
+int				sort_large_stacks(t_stack *stack_a, t_stack *stack_b);
+t_stack			find_next_(const t_stack *stack_, t_stack elemnt, char *type);
 
 /*=========== (push_swap functions) ===========*/
-void	ft_putstr_fd(char *str, int fd);
-void	free_all(void *stack_a, void *stack_b);
-void	free_recursively(char **str);
+void			ft_putstr_fd(char *str, int fd);
+void			free_all(void *stack_a, void *stack_b);
+void			free_recursively(char **str);
 
 #endif
